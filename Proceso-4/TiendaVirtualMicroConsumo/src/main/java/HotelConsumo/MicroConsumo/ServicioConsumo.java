f75fcee4f1329b5088a8d446ed715d56b7c8a204
Styngixx <francisinchee@gmail.com>
2026-06-25 22:59:14 -0500

    BoePastaChatPeroQueMandanQuieroQueque

--- Proceso-4/TiendaVirtualMicroConsumo/src/main/java/HotelConsumo/MicroConsumo/ServicioConsumo.java
+++ Proceso-4/TiendaVirtualMicroConsumo/src/main/java/HotelConsumo/MicroConsumo/ServicioConsumo.java
@@ -1,4 +1,5 @@
 package HotelConsumo.MicroConsumo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
@@ -11,7 +12,23 @@ public class ServicioConsumo {
     public FichaConsumo registrarConsumo(FichaConsumo ficha) {
         ficha.setFechaHora(LocalDateTime.now());
         ficha.setId(r.findMaxId() + 1);
+
+        double total = 0;
+        if (ficha.getServicios() != null) {
+            for (ServicioAdquirido s : ficha.getServicios()) {
+                total += s.getPrecio();
+            }
+        }
+        ficha.setImporteTotal(total);
         return r.save(ficha);
     }
-    public List<FichaConsumo> listar() { return r.findAll(); }
+
+    public List<FichaConsumo> listar() { 
+        return r.findAll(); 
+    }
+
+    public FichaConsumo buscarPorId(Long id) {
+        // Asegúrate de que esto sea r.findById y no un throw exception
+        return r.findById(id).orElse(null);
+    }
 }
